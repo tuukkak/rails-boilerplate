f4ed11e64d696a1bd51c1ef53f29d06c156f48f2
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-09-25 20:01:30 +0300

    Add api authentication endpoints

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::AuthenticationController < Api::V1::BaseController
+  def sign_up
+  end
+
+  def sign_in
+  end
+
+  def facebook_sign_in
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resource :authentication, only: [], controller: "authentication" do
+        collection do
+          post :sign_up
+          post :sign_in
+          post :facebook_sign_in
+        end
+      end
+    end
+  end
 end
